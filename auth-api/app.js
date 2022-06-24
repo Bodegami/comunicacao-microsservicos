43a1f9943aa1b77801fa2918898b522274d4fe8e
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-23 21:11:54 -0300

    auth-db - criando o dbConfig

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,14 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
-app.get('/api/status', (req, res) => {
-  return res
-  .status(200).json({
-    service: 'Auth-API',
-    status: 'up',
+app.get("/api/status", (req, res) => {
+  return res.status(200).json({
+    service: "Auth-API",
+    status: "up",
     httpStatus: 200,
   });
 });
