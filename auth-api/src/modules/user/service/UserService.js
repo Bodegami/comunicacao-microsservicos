1809134f8224d40b978bd93eed5b1ef751303a6b
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-06-27 11:45:24 -0300

    auth-api - criando exceptions, controller e routes

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,5 +1,6 @@
-import userRepository from "../repository/userRepository";
+import userRepository from "../repository/userRepository.js";
 import * as httpStatus from "../../../config/constants/httpStatus.js";
+import UserException from "../exception/UserException.js";
 
 class UserService {
 
@@ -8,32 +9,35 @@ class UserService {
     try {
       const {email} = req.params;
       this.validateRequestData(email);
+      let user = await userRepository.findByEmail(email);
+      this.validateUserNotFound(user);
 
-      let user = userRepository.findByEmail(email);
-      if (!user) {
-
-      }
       return {
         status: httpStatus.SUCCESS,
         user: {
           id: user.id,
           name: user.name,
           email: user.email,
-        }
-      }
-
+        },
+      };
     } catch(err) {
       return {
-        status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
-        message: err.status,
+        status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+        message: err.message,
       };
     }
-
   }
 
   validateRequestData(email) {
     if (!email) {
-      throw new Error("User email was not informed!")
+      throw new UserException(httpStatus.BAD_REQUEST, 
+        "User email was not informed!")
+    }
+  }
+
+  validateUserNotFound(user) {
+    if (!user) {
+      throw new Error(httpStatus.BAD_REQUEST, "User was not found..");
     }
   }
 
