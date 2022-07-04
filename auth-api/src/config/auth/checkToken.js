efddfedc378413a734b2c2e2070be6269fbc90e1
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-07-03 22:01:53 -0300

    Auth-api - adicionando o middleware de autenticacao

---
+++ auth-api/src/config/auth/checkToken.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import { promisify } from "util";
+
+import AuthException from "./AuthException.js";
+
+import * as secrets from "../constants/secrets.js";
+import * as httpStatus from "../constants/httpStatus.js";
+
+const emptySpace = " ";
+
+export default async (req, res, next) => {
+  try {
+    let { authorization } = req.headers;
+    if (!authorization) {
+      throw new AuthException(httpStatus.UNAUTHORIZED, "Acess token was not informed!");
+    }
+    let acessToken = authorization;
+    if (acessToken.includes(emptySpace)) {
+      acessToken = acessToken.split(emptySpace)[1];
+    } else {
+      acessToken = authorization;
+    }
+    const decoded = await promisify(jwt.verify)(
+      acessToken, 
+      secrets.API_SECRET
+      );
+    req.authUser = decoded.authUser;
+    return next();
+  } catch(err) {
+    const status = err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR;
+    return res.status(status).json({ status, message: err.message });  
+  }
+}
